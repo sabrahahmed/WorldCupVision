cb66230e5f5ba3d6ee81b1428f9ae8fbafd855c3
sabrahmed <ahmedsabrah@hotmail.com>
2023-03-13 15:36:36 -0400

    complete

--- js/result.js
+++ js/result.js
@@ -3,7 +3,7 @@
     const { team, ranking, dateQualified, qualificationMethod, lastQualification, previousTitles } = teamInfo;
 
     const imgEl = document.querySelector(".result-container img");
-    imgEl.setAttribute("src", `images/flags/${team}.jpg`);
+    imgEl.setAttribute("src", `images/Flags/${team}.jpg`);
         
     document.getElementById("country").innerHTML = team;
     document.getElementById("rank").innerHTML = `World Ranking: ${ranking}`;
